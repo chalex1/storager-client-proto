8d3107c30734d39af5fd51e187734ad4baa4996f
chalexIAC <lbw@yandex.ru>
2016-09-28 18:21:14 +0300

    Frontend Mocks changes

--- frontend-mock/main.js
+++ frontend-mock/main.js
@@ -1,4 +1,18 @@
 $(function () {
 
 	$(".dropdown-button").dropdown();
+	$('select').material_select();
+	$('.datepicker').pickadate({
+		selectMonths: true, // Creates a dropdown to control month
+		selectYears: 15 // Creates a dropdown of 15 years to control year
+	});
+  
+    $('input.autocomplete').autocomplete({
+    data: {
+		  "СПб ГУП СПб ИАЦ": null,
+		  "Какая-то другая организация": null,
+		  "Организация 1": null,
+		  "Организация 27": null
+		}
+	});
 });
