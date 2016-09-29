bc2233fd57b09afde43c4dade231a7b92d6cf41a
chalexIAC <lbw@yandex.ru>
2016-09-29 18:00:28 +0300

    don't like it, but some moments to think

--- frontend-mock/main.js
+++ frontend-mock/main.js
@@ -1,11 +1,14 @@
 $(function () {
 
-	$(".dropdown-button").dropdown();
+	$(".dropdown-button").dropdown({	
+		belowOrigin: true // Displays dropdown below the button
+	});
 	$('select').material_select();
 	$('.datepicker').pickadate({
 		selectMonths: true, // Creates a dropdown to control month
 		selectYears: 15 // Creates a dropdown of 15 years to control year
 	});
+	//$('.collapsible').collapsible();
   
     $('input.autocomplete').autocomplete({
     data: {
