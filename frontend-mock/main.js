694f1b598b50973ea0b964fa7fa233743bb33605
chalexIAC <lbw@yandex.ru>
2016-09-30 17:02:19 +0300

    extra thoughts about indicator tree and additional detailed modes in a dialog

--- frontend-mock/main.js
+++ frontend-mock/main.js
@@ -8,7 +8,10 @@ $(function () {
 		selectMonths: true, // Creates a dropdown to control month
 		selectYears: 15 // Creates a dropdown of 15 years to control year
 	});
-	//$('.collapsible').collapsible();
+	$('.collapsible').collapsible({
+		accordion: true
+	});
+	$('.modal-trigger').leanModal();
   
     $('input.autocomplete').autocomplete({
     data: {
